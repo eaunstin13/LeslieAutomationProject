65b3ea2b307f607838b702b01e82a778da1cc90c
Edmund Aunstin <edmund.aunstin@nulogic.io>
2024-03-13 23:36:45 +0530

    Added below features - Added feature to run in CICD tool

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -137,6 +137,8 @@ public class HomePage extends ProjMethods {
 	
 	public void verifyHomePage() throws AWTException, InterruptedException {
 
+		String MyTestVariable="null";
+		System.out.println(MyTestVariable);
 		Thread.sleep(10);
 		//click(closeIcon);
 		getDriver().navigate().refresh();
